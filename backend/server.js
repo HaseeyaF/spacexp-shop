1e8fb9c07395f034d83afb8ccc35bf092d2d4b5a
HFarvin <haseeyaf@gmail.com>
2025-11-07 18:47:47 +0530

    CORS added

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,22 @@ const productsRoutes = require("./routes/products");
 const adsRoutes = require("./routes/ads");
 const wishlistRoutes = require("./routes/wishlist");
 const reviewsRoutes = require("./routes/reviews");
+const orderRoutes = require('./routes/orders');
+const promoRoutes = require('./routes/promos');
+
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // routes
@@ -20,15 +33,19 @@ app.use("/api/products", productsRoutes);
 app.use("/api/ads", adsRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/reviews", reviewsRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/promos', promoRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Backend running on ${PORT}`));
-  })
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
