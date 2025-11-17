431402eb87b578a144af418951ccefbf36f819c2
HFarvin <haseeyaf@gmail.com>
2025-11-17 23:20:16 +0530

    HelmetProvider added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,12 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./i18n"; // Import i18n configuration
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </BrowserRouter>
 );
