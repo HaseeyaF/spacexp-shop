27b167a761417026a0427007baf7c987565f1d55
HFarvin <haseeyaf@gmail.com>
2025-11-19 22:57:06 +0530

    Display Products in 5 Items per Ro

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -98,9 +98,9 @@ export default function Products() {
                 No products found for selected filters.
               </p>
             ) : (
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {products.map((p) => (
-                  <ProductCard key={p._id} product={p} />
+              <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+                {products.map((product) => (
+                  <ProductCard key={product._id} product={product} />
                 ))}
               </div>
             )}
